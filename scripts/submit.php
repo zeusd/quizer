2dafa0fadcd9e22dca43047902ea1b66f9031eb2
Joro Dimov <thegdimov@gmail.com>
2026-01-26 19:35:03 +0200

    Finish result logic

--- scripts/submit.php
+++ scripts/submit.php
@@ -1,7 +1,24 @@
 <?php
 
+$uid_len = 12;
+
+function rand_uid($len) {
+    $chars = '0123456789_-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $char_len = strlen($chars);
+    $rand_str = '';
+
+    for ($i = 0; $i < $len; $i++) {
+        $rand_ind = random_int(0, $char_len - 1);
+        $rand_str .= $chars[$rand_ind];
+    }
+
+    return $rand_str;
+}
+
 $json_req = file_get_contents('php://input');
 $req = json_decode($json_req, true);
+$quiz = $req['quiz'];
+$ans = $req['answers'];
 
 $servername = "localhost";
 $username = "root";
@@ -15,30 +32,41 @@ if (!$conn) {
 
 #   echo $req['quiz'] . "<br>";
 
-$ans_sql = "SELECT * FROM quizzer.quizzes WHERE NAME=\"" . $req['quiz'] . "\";";
 $score = 0;
-
-#   echo $ans_sql . "<br>";
-
-
+$ans_sql = "SELECT * FROM quizzer.quizzes WHERE NAME=\"" . $quiz . "\";";
 $res_ans = $conn->query($ans_sql);
 
 if ($res_ans->num_rows > 0) {
-    while ($row = $res_ans->fetch_assoc()) {
-        $ans = json_decode($row["ANSWERS"]);
-        $max = $row["MAX_SCORE"];
-        foreach ($ans as $k => $v) {
+    $row = $res_ans->fetch_assoc();
+    $q_ans = json_decode($row["ANSWERS"]);
+    $max = $row["MAX_SCORE"];
+    foreach ($q_ans as $k => $v) {
 #           echo $k . " : " . $v . "<br>";
-            if ($req['answers']['q' . $k] && $req['answers']['q' . $k] == $v) {
+        if ($ans['q' . $k] && $ans['q' . $k] == $v) {
 #               echo ".<br>";
-                $score++;
-            }
+            $score++;
         }
     }
 }
 
-$res = array('uid' => $score);
+$uid = rand_uid($uid_len);
+$ch_sql = "SELECT * FROM quizzer.results WHERE UID=\"" . $uid . "\";";
+$ch_res = $conn->query($ch_sql);
 
+while (mysqli_num_rows($ch_res) != 0) {
+    $uid = rand_uid($uid_len);
+    $ch_sql = "SELECT * FROM quizzer.results WHERE UID=\"" . $uid . "\";";
+    $ch_res = $conn->query($ch_sql);
+}
+
+#   echo $ch_sql . "<br>";
+
+$ins_sql = "INSERT INTO quizzer.results (UID, QUIZ, ANSWERS, SCORE) VALUES ('" . $uid . "', '" . $quiz . "', '" . json_encode($ans) . "', '" . $score . "');";
+$conn->query($ins_sql);
+
+#   echo $ins_sql . "<br>";
+
+$res = array('uid' => $uid);
 echo json_encode($res);
 
 mysqli_close($conn);
